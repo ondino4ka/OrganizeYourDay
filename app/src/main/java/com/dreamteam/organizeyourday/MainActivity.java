88478949718fe49a3f4d69088c49f9e76aed02be
mkinitcpio <wot220697@gmail.com>
2016-03-08 17:05:01 +0300

    Add navigationBar

--- app/src/main/java/com/dreamteam/organizeyourday/MainActivity.java
+++ app/src/main/java/com/dreamteam/organizeyourday/MainActivity.java
@@ -1,8 +1,12 @@
 package com.dreamteam.organizeyourday;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +18,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         if(toolbar != null){
             setSupportActionBar(toolbar);
+           getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         }
 
-
+       Drawer result = new DrawerBuilder()
+               .withActivity(this)
+               .withToolbar(toolbar)
+               .withActionBarDrawerToggleAnimated(true)
+               .build();
     }
 }
