3d56b7ad73c03cda161916ce5374876175313c8a
mkinitcpio <wot220697@gmail.com>
2016-03-08 22:15:45 +0300

    Add icons in navigationBar

--- app/src/main/java/com/dreamteam/organizeyourday/MainActivity.java
+++ app/src/main/java/com/dreamteam/organizeyourday/MainActivity.java
@@ -11,6 +11,7 @@ import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
@@ -43,21 +44,20 @@ public class MainActivity extends AppCompatActivity {
                 .withHeaderBackground(R.drawable.head_wallpaper)
                 .build();
 
-        final PrimaryDrawerItem item1 =  new PrimaryDrawerItem().withName(R.string.nav_bar_home);
-        final SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName(R.string.nav_bar_settings);
-        final SecondaryDrawerItem item3 = new SecondaryDrawerItem().withName(R.string.nav_bar_about);
-
+        final PrimaryDrawerItem item1 =  new PrimaryDrawerItem().withName(R.string.nav_bar_home).withIcon(R.drawable.ic_home_black_24dp);
+        final SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName(R.string.nav_bar_settings).withIcon(R.drawable.ic_build_black_24dp);
+        final SecondaryDrawerItem item3 = new SecondaryDrawerItem().withName(R.string.nav_bar_about).withIcon(R.drawable.ic_feedback_black_24dp);
         Drawer result = new DrawerBuilder()
                 .withAccountHeader(account)
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .withActionBarDrawerToggleAnimated(true)
                 .addDrawerItems(
-                        item1, item2, item3
+                        item1, new DividerDrawerItem(), item2, item3
                 ).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        if (drawerItem == item2) {
+                        if (drawerItem == item2){
                             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                             startActivity(intent);
                         }
