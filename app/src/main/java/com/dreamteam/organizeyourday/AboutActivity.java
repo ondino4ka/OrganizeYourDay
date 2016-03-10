aca8319add39d95f2be82021c6800f73a4fbae11
axe607 <maxim-dollor@yandex.by>
2016-03-10 20:07:46 +0300

    Edited AboutActivity

--- app/src/main/java/com/dreamteam/organizeyourday/AboutActivity.java
+++ app/src/main/java/com/dreamteam/organizeyourday/AboutActivity.java
@@ -1,31 +1,49 @@
 package com.dreamteam.organizeyourday;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity{
 
-    private int count = 0;
+    private byte count = 0;
+    String rep_url = "https://github.com/mkinitcpio/OrganizeYourDay";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
 
+
+    }
+
+    public void jumpToRepository(View view)
+    {
+        Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse(rep_url));
+        startActivity(browser);
     }
 
     public void setTextAbout(View view) {
 
-        TextView aboutText = (TextView)findViewById(R.id.textAbout);
-        RelativeLayout relativeLayout =(RelativeLayout) findViewById(R.id.aboutLayout);
 
-        aboutText.setText("Это прога просто " + count + "/10");
-        if(count>=10)
+        TextView aboutText = (TextView)findViewById(R.id.textRepository);
+        RelativeLayout relativeLayout =(RelativeLayout) findViewById(R.id.aboutLayout);
+        aboutText.setText("Checkout our repository:\n " + rep_url);
+
+        count++;
+        if(count==15)
         {
             relativeLayout.setBackgroundResource(R.drawable.hack_0);
         }
-        count++;
+        if(count>17)
+        {
+            relativeLayout.setBackgroundResource(R.color.colorBackground);
+            count =0;
+        }
     }
 }
