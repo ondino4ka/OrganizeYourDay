3c6cb50334897142f1c9b5da9788342b882a1a6b
axe607 <maxim-dollor@yandex.by>
2016-03-10 21:07:19 +0300

    Edited SettingsActivity/ Add Toolbar in AboutActivity and SettingsActivity

--- app/src/main/java/com/dreamteam/organizeyourday/AboutActivity.java
+++ app/src/main/java/com/dreamteam/organizeyourday/AboutActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Spanned;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
 public class AboutActivity extends AppCompatActivity{
 
+    Toolbar toolbar;
     private byte count = 0;
     String rep_url = "https://github.com/mkinitcpio/OrganizeYourDay";
 
@@ -18,6 +30,10 @@ public class AboutActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        generateToolbar();
+        generateNavigationDrawer();
 
 
     }
@@ -45,5 +61,68 @@ public class AboutActivity extends AppCompatActivity{
             relativeLayout.setBackgroundResource(R.color.colorBackground);
             count =0;
         }
+    }
+
+
+    private void generateToolbar() {
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }
+    }
+
+    private void generateNavigationDrawer() {
+
+        final PrimaryDrawerItem homeButton =  new PrimaryDrawerItem()
+                .withName(R.string.nav_bar_home)
+                .withIcon(R.drawable.ic_home_black_24dp);
+        final SecondaryDrawerItem settingsButton = new SecondaryDrawerItem()
+                .withName(R.string.nav_bar_settings)
+                .withIcon(R.drawable.ic_build_black_24dp);
+        final SecondaryDrawerItem aboutButton = new SecondaryDrawerItem()
+                .withName(R.string.nav_bar_about)
+                .withIcon(R.drawable.ic_feedback_black_24dp);
+        final DividerDrawerItem devider = new DividerDrawerItem();
+
+        AccountHeader account = accountHeader();
+
+        Drawer navigationDrawer = new DrawerBuilder()
+                .withAccountHeader(account)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        homeButton,
+                        devider,
+                        settingsButton,
+                        aboutButton
+                ).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        if (drawerItem.equals(homeButton)){
+                            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }else if (drawerItem.equals(settingsButton)) {
+                            Intent intent = new Intent(AboutActivity.this, SettingsActivity.class);
+                            startActivity((intent));
+                        }
+                        return false;
+                    }
+                })
+                .build();
+    }
+    private AccountHeader accountHeader(){
+        AccountHeader account = new AccountHeaderBuilder()
+                .withActivity(this)
+                .addProfiles(
+                        new ProfileDrawerItem()
+                                .withName("DreamTeam")
+                                .withEmail("dreamTeam@gmail.com")
+                )
+                .withHeaderBackground(R.drawable.head_wallpaper)
+                .build();
+        return account;
     }
 }
--- app/src/main/java/com/dreamteam/organizeyourday/SettingsActivity.java
+++ app/src/main/java/com/dreamteam/organizeyourday/SettingsActivity.java
@@ -1,11 +1,99 @@
 package com.dreamteam.organizeyourday;
 
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 
 public class SettingsActivity extends AppCompatActivity{
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        generateToolbar();
+        generateNavigationDrawer();
 
 
+    }
+
+
+    private void generateToolbar() {
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }
+    }
+
+    private void generateNavigationDrawer() {
+
+        final PrimaryDrawerItem homeButton =  new PrimaryDrawerItem()
+                .withName(R.string.nav_bar_home)
+                .withIcon(R.drawable.ic_home_black_24dp);
+        final SecondaryDrawerItem settingsButton = new SecondaryDrawerItem()
+                .withName(R.string.nav_bar_settings)
+                .withIcon(R.drawable.ic_build_black_24dp);
+        final SecondaryDrawerItem aboutButton = new SecondaryDrawerItem()
+                .withName(R.string.nav_bar_about)
+                .withIcon(R.drawable.ic_feedback_black_24dp);
+        final DividerDrawerItem devider = new DividerDrawerItem();
+
+        AccountHeader account = accountHeader();
+
+        Drawer navigationDrawer = new DrawerBuilder()
+                .withAccountHeader(account)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        homeButton,
+                        devider,
+                        settingsButton,
+                        aboutButton
+                ).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        if (drawerItem.equals(homeButton)){
+                            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }else if (drawerItem.equals(aboutButton)) {
+                            Intent intent = new Intent(SettingsActivity.this, AboutActivity.class);
+                            startActivity((intent));
+                        }
+                        return false;
+                    }
+                })
+                .build();
+    }
+    private AccountHeader accountHeader(){
+        AccountHeader account = new AccountHeaderBuilder()
+                .withActivity(this)
+                .addProfiles(
+                        new ProfileDrawerItem()
+                                .withName("DreamTeam")
+                                .withEmail("dreamTeam@gmail.com")
+                )
+                .withHeaderBackground(R.drawable.head_wallpaper)
+                .build();
+        return account;
+    }
 
 }
